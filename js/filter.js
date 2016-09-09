c6709f77777d103f601dd8330a58ba3cb358b65a
Yosuke Hishinuma <yosukeonrails@gmail.com>
2016-09-08 18:59:09 -0700

    added some more bible books

--- js/filter.js
+++ js/filter.js
@@ -1,26 +1,25 @@
 
    function chapterFilter(book ){
 
-  $('.chapter li').remove()
-   	  
-   	if (book=='romans1'){ chapter_number= 16; book='romans'} 
-   	else if (book=='ps1'){ chapter_number= 150; book='ps'}
-	else if (book=='jn1'){ chapter_number=21; book='jn'}
+      $('.chapter li').remove()
+        
+      if (book=='gen1'){ chapter_number= 50; book='genesis'} 
+      else if (book=='romans1'){ chapter_number= 16; book='romans'} 
+      else if (book=='ps1'){ chapter_number= 150; book='ps'}
+      else if (book=='jn1'){ chapter_number=21; book='jn'}
 
-		console.log(chapter_number)
+      console.log(chapter_number)
 
-		console.log(book)
+      console.log(book)
 
-	i=0
+      i=0
 
+      for (i = 0; i <= chapter_number; i++) { 
 
+      $('.chapter').append('<li>'+i+'</li>')
 
-		for (i = 0; i <= chapter_number; i++) { 
-    
-    $('.chapter').append('<li>'+i+'</li>')
-
-}
-   }
+      }
+    }
 
 
    
